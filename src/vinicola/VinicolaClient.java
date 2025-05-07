b8e0dcb2e9fbd19d48a4f0d4fe0660738098dd0b
Leticia Resina <80417466+letyresina@users.noreply.github.com>
2025-05-07 11:48:57 -0300

    Ajeitando o vinicolaclient

--- src/vinicola/VinicolaClient.java
+++ src/vinicola/VinicolaClient.java
@@ -6,8 +6,8 @@ import java.net.URL;
 
 public class VinicolaClient {
     public static void main(String[] args) throws Exception{
-        URL url = new URL("http://127.0.0.1:9876/vinicolawsdL");
-        QName qName = new QName("http://vinicola/", "VinicolaServerImpl");
+        URL url = new URL("http://127.0.0.1:9876/vinicola?wsdl");
+        QName qName = new QName("http://vinicola/", "VinicolaServerImplService");
         Service ws = Service.create(url, qName);
         VinicolaServer vinicolaServer = ws.getPort(VinicolaServer.class);
         System.out.println("Produto cadastrado: " + vinicolaServer.cadastrarVinho(1L, "Vinho Teste", 70.00F));
